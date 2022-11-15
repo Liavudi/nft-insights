f0ad73ce5ee52b5a2eb8a86693dde47a2b50c770
Liavudi <liavudi@gmail.com>
2022-11-15 14:39:12 +0200

    built a skeleton for the implementaion

--- main.py
+++ main.py
@@ -6,9 +6,27 @@ from utils.ether_scan import get_etherscan_client
 
 # Connect to etherscan and elastic search
 es = get_elasticsearch_client()
-etherscan = get_etherscan_client()
+eth = get_etherscan_client()
 app = Flask(__name__)
 
+# built a skeleton for the implementation
+def get_gas_fee_for_transaction(block: int):
+    pass
+
+def get_average_contracts_gas_fee(start_timestamp: float, end_timestamp: float):
+    #TODO convert the timestamp to a specific block number
+    starting_block = eth.get_block_number_by_timestamp(timestamp=start_timestamp, closest="before")
+    ending_block = eth.get_block_number_by_timestamp(timestamp=end_timestamp, closest="after")
+    #TODO get block number by one time after and one time before
+    blocks = []
+
+    #TODO access the each block and take the transaction info
+    for block in blocks:
+        get_gas_fee_for_transaction(block)
+
+    #TODO sum up all the gas prices at each block and calculate the average fee for this timestamp
+print(eth.get_block_number_by_timestamp(timestamp=round(1660741244461 * 1000), closest='before'))
+print(eth.get_proxy_block_by_number(hex(15975347))['transactions'])
 
 @app.route('/')
 def homepage():
--- utils/ether_scan.py
+++ utils/ether_scan.py
@@ -5,4 +5,4 @@ from utils.common import get_mandatory_env_variable
 
 def get_etherscan_client():
     etherscan_api_key = get_mandatory_env_variable('ETHERSCAN_API_KEY')
-    return Etherscan(etherscan_api_key)
+    return  Etherscan(etherscan_api_key)
