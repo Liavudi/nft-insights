dafcd54a6f31f3363c8e158b2bd4ee9416f9e1c6
Liavudi <liavudi@gmail.com>
2022-11-15 16:02:25 +0200

    added my own sdk for etherscan and get blocks is working

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
 from flask import Flask, render_template
 
-
+from utils.common import get_mandatory_env_variable
 from utils.elastic_search import get_elasticsearch_client
-from utils.ether_scan import get_etherscan_client
+from utils.ether_scan import EtherscanAPIClient
 
 # Connect to etherscan and elastic search
 es = get_elasticsearch_client()
-eth = get_etherscan_client()
+eth = EtherscanAPIClient(address="0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB", api_key=get_mandatory_env_variable('ETHERSCAN_API_KEY'))
 app = Flask(__name__)
 
-# built a skeleton for the implementation
+
 def get_gas_fee_for_transaction(block: int):
     pass
 
-def get_average_contracts_gas_fee(start_timestamp: float, end_timestamp: float):
-    #TODO convert the timestamp to a specific block number
-    starting_block = eth.get_block_number_by_timestamp(timestamp=start_timestamp, closest="before")
-    ending_block = eth.get_block_number_by_timestamp(timestamp=end_timestamp, closest="after")
-    #TODO get block number by one time after and one time before
-    blocks = []
+def get_eth_price_for_timestamp():
+    pass
 
-    #TODO access the each block and take the transaction info
+def get_average_contracts_gas_fee(from_timestamp: float, to_timestamp: float):
+    #TODO convert the timestamp to a specific block number
+    from_block, to_block = eth.get_blocks_by_timestamp(from_timestamp=from_timestamp, to_timestamp=to_timestamp)
+    
+    #TODO get log from starting block to ending block
+    print(eth.get_logs())
+    sum = 0
+    blocks =[]
     for block in blocks:
+        #TODO convert the gasPrice from the log by multiplying in price of eth in that time
         get_gas_fee_for_transaction(block)
 
     #TODO sum up all the gas prices at each block and calculate the average fee for this timestamp
-print(eth.get_block_number_by_timestamp(timestamp=round(1660741244461 * 1000), closest='before'))
-print(eth.get_proxy_block_by_number(hex(15975347))['transactions'])
+
+print(eth.get_blocks_by_timestamp(from_timestamp="1636984662", to_timestamp="1668520662"))
+# print(eth.get_proxy_block_by_number(hex(15975347))['transactions'])
 
 @app.route('/')
 def homepage():
--- utils/ether_scan.py
+++ utils/ether_scan.py
@@ -1,8 +1,71 @@
-from etherscan import Etherscan
-
+import requests
+from typing import List, Dict, Tuple
 from utils.common import get_mandatory_env_variable
+import logging
 
 
-def get_etherscan_client():
-    etherscan_api_key = get_mandatory_env_variable('ETHERSCAN_API_KEY')
-    return  Etherscan(etherscan_api_key)
+class EtherscanAPIClient:
+
+    API_URL = 'https://api.etherscan.io/api'
+
+    def __init__(self, address: str, api_key: str):
+
+        self._address = address
+        self._api_key = api_key
+
+    def get_blocks_by_timestamp(self, from_timestamp: str, to_timestamp: str) -> Tuple[str, str]:
+        try:
+            # Get from block
+            from_block_response = requests.get(url=self.API_URL,
+                                               params={
+                                                   'module': 'block',
+                                                   'action': 'getblocknobytime',
+                                                   'timestamp': from_timestamp,
+                                                   'closest': 'after',
+                                                   'apiKey': self._api_key
+                                               })
+            from_block = from_block_response.json()['result']
+
+            # Get to block
+            to_block_response = requests.get(url=self.API_URL,
+                                             params={
+                                                 'module': 'block',
+                                                 'action': 'getblocknobytime',
+                                                 'timestamp': to_timestamp,
+                                                 'closest': 'before',
+                                                 'apiKey': self._api_key
+                                             })
+            to_block = to_block_response.json()['result']
+
+        except Exception as exc:
+            logging.error(f'Failed to get blocks by timestamp. exc {exc}')
+            raise exc
+
+        return from_block, to_block
+
+    def get_logs(self, contract_address: str, from_block: int, to_block: int) -> List[Dict]:
+        try:
+            # TODO: support more pages in the future
+            transactions_response = requests.get(url=self.API_URL,
+                                                 params={
+                                                     'module': 'logs',
+                                                     'action': 'getLogs',
+                                                     'address': contract_address,
+                                                     'fromBlock': from_block,
+                                                     'toBlock': to_block,
+                                                     'page': '1',
+                                                     'offset': '1000',
+                                                     'apiKey': self._api_key
+                                                 })
+        except Exception as exc:
+            logging.error(f'Failed to get transactions. exc {exc}')
+            raise exc
+
+        return transactions_response.json()['result']
+
+    def get_logs_by_timestamp(self, contract_address: str, from_timestamp: str, to_timestamp: str):
+        from_block, to_block = self.get_blocks_by_timestamp(
+            from_timestamp, to_timestamp)
+        return self.get_logs(contract_address=contract_address, from_block=from_block, to_block=to_block)
+
+
