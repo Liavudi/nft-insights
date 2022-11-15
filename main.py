99cee8bea03d06be86d87ed211ec4a329aff4f6e
Liavudi <liavudi@gmail.com>
2022-11-15 18:06:54 +0200

    progress so far elastic not working properly

--- main.py
+++ main.py
@@ -12,12 +12,7 @@ es_client = ElasticSearchClient(username=get_mandatory_env_variable('ELASTIC_USE
                          cloud_id="elastic:dXMtY2VudHJhbDEuZ2NwLmNsb3VkLmVzLmlvJDllMDg2NzdiMDkzMjRmMGJiMzY2MDBmYWFhYTAwZDNiJGExMjVmNWU4N2ZlNTRjZTBhYzcwNWQ4YTQ4YTU3NmM0")
 eth_client = EtherscanAPIClient(api_key=get_mandatory_env_variable('ETHERSCAN_API_KEY'))
 app = Flask(__name__)
-
-def save_eth_prices_in_usd_to_elasticsearch(List[str, float]):
-    return None
-
-def get_eth_in_usd():
-    pass
+    
 
 def get_average_contracts_gas_fee(from_timestamp: float, to_timestamp: float):
     #TODO convert the timestamp to a specific block number
@@ -29,14 +24,14 @@ def get_average_contracts_gas_fee(from_timestamp: float, to_timestamp: float):
     for transaction_log in transaction_logs:
         # TODO convert the gasPrice from the log by multiplying in price of eth in that time
         gas_price_in_eth = decode_from_hex(transaction_log['gasPrice'])
-        eth_in_usd = get_eth_in_usd(decode_from_hex(transaction_log['timeStamp']))
+        eth_in_usd = es_client.get_eth_price_in_usd(timestamp=decode_from_hex(transaction_log['timeStamp']))
         sum += eth_in_usd * gas_price_in_eth
 
     average_price = sum / len|(transaction_logs)
     return average_price
 
     #TODO sum up all the gas prices at each block and calculate the average fee for this timestamp
-get_average_contracts_gas_fee('1636984662','1668520662' )
+# get_average_contracts_gas_fee('1636984662','1668520662' )
 # print(eth.get_blocks_by_timestamp(from_timestamp="1636984662", to_timestamp="1668520662"))
 # print(eth.get_proxy_block_by_number(hex(15975347))['transactions'])
 
@@ -47,4 +42,6 @@ def homepage():
 
 if __name__ == '__main__':
     es_client.create_eth_to_usd_index()
+    es_client.populate_eth_to_usd_index()
+    eth_in_usd = es_client.get_eth_price_in_usd(timestamp='1660770085815')
     app.run(debug=True)
--- utils/elastic_search.py
+++ utils/elastic_search.py
@@ -1,18 +1,65 @@
+import json
 import requests
 
 from elasticsearch import Elasticsearch
 
-from utils.common import get_mandatory_env_variable
-
 
 class ElasticSearchClient:
+    ETH_TO_USD_INDEX_NAME = 'eth_to_usd_mapping'
+
     def __init__(self, cloud_id: str, username: str, password: str) -> None:
-        self._client = Elasticsearch(cloud_id=cloud_id, http_auth=(username, password))
+        self._client = Elasticsearch(
+            cloud_id=cloud_id, http_auth=(username, password))
 
     def create_index(self, name: str, request_body: dict):
-        self._client.indices.create(index=name, body=request_body)
+        index_exists = self._client.indices.exists(index=name)
+        if index_exists == False:
+            self._client.indices.create(index=name, body=request_body)
 
     def create_eth_to_usd_index(self):
+        request_body = {
+            'mappings': {
+                'properties': {
+                    'timeStamp': {'type': 'text'},
+                    'price': {'type': 'float'}
+                }
+            }
+        }
+        self.create_index(self.ETH_TO_USD_INDEX_NAME,
+                          request_body=request_body)
+
+    def populate_eth_to_usd_index(self):
         eth_to_usd_response = requests.get('https://www.coingecko.com/price_charts/279/usd/90_days.json')
         parsed_eth_to_usd_list = eth_to_usd_response.json()['stats']
-        self.index(index='eth_to_usd', document=parsed_eth_to_usd_list)
+        bulk_list = []
+        for eth_to_usd_timestamp, eth_to_usd_price in parsed_eth_to_usd_list:
+            action = json.dumps({
+                'index': {
+                    '_index': self.ETH_TO_USD_INDEX_NAME,
+                    '_id': int(eth_to_usd_timestamp),
+                    'type': 'text'
+                }
+            })
+            item = json.dumps({
+                "timeStamp": eth_to_usd_timestamp,
+                "price": eth_to_usd_price
+            })
+            bulk_list.append(action)
+            bulk_list.append(item)
+        #TODO need to solve this, it is not working, receiving bad request need to tweak the request to make it work
+        self._client.bulk(index=self.ETH_TO_USD_INDEX_NAME,
+                          operations=item)
+
+    def get_eth_price_in_usd(self, timestamp: str):
+        result = self._client.search(index=self.ETH_TO_USD_INDEX_NAME, query={
+            'bool': {
+                'filter': [{
+                    'range': {
+                        'timeStamp': {
+                            'lte': timestamp
+                        }
+                    }
+                }]
+            }
+        }, size=1, filter_path=['hits.max_score'])
+        print(result)
