efb1651032aca205a3b25a0e13f5877fecf720ca
Liavudi <liavudi@gmail.com>
2022-11-15 19:20:44 +0200

    last commit tried to implement a hack using a list instead of elasticsearch

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ state_helper = StateHelper()
 app = Flask(__name__)
 
 
-def get_average_contracts_gas_fee(from_timestamp: float, to_timestamp: float):
+def get_average_contracts_gas_fee(contract_address: str, from_timestamp: float, to_timestamp: float):
     # TODO convert the timestamp to a specific block number
     from_block, to_block = eth_client.get_blocks_by_timestamp(
         from_timestamp=from_timestamp, to_timestamp=to_timestamp)
 
     # TODO get log from starting block to ending block
     transaction_logs = eth_client.get_logs(
-        contract_address='0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB', from_block=from_block, to_block=to_block)
+        contract_address=contract_address, from_block=from_block, to_block=to_block)
     sum = 0
     for transaction_log in transaction_logs:
         # TODO convert the gasPrice from the log by multiplying in price of eth in that time
@@ -36,12 +36,22 @@ def get_average_contracts_gas_fee(from_timestamp: float, to_timestamp: float):
     return average_price
 
 
+# def calculate_contract_average_gas_fee(contract_name: str, contract_address: str, days_ago: int = 90):
+#     from_timestamp = get_timestamp_in_seconds(days_ago=days_ago)
+#     get_average_contracts_gas_fee(contract_address=contract_address, from_timestamp=from_timestamp, to_timestamp=to_timestamp)
+
 @app.route('/')
 def homepage():
+    return render_template('index.html')
+
+@app.route('/contract-average-gas-fee')
+def contract_average_gas_fee():
     return render_template('index.html')
 
 
 if __name__ == '__main__':
     # es_client.create_eth_to_usd_index()
     # es_client.populate_eth_to_usd_index()
+    contracts = ['0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB', '0x60e4d786628fea6478f785a6d7e704777c86a7c6']
+
     app.run(debug=True)
